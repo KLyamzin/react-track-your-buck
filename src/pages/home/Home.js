cfc04cbbb29848e277c7fa0588ae7aaea0cadb28
Kirill Lyamzin <kiryanoff88@gmail.com>
2022-09-22 18:39:21 -0400

    can add items to firestore

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,6 +1,14 @@
 import styles from './Home.module.css';
 import React from 'react';
+import TransactionForm from './TransactionForm';
 
 export default function home() {
-  return <div>home</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.content}></div>
+      <div className={styles.sidebar}>
+        <TransactionForm />
+      </div>
+    </div>
+  );
 }
