434237418bb628bdd43c7f5efb49101bb33a0cf3
Kirill Lyamzin <kiryanoff88@gmail.com>
2022-09-21 20:28:07 -0400

    created home, signup, login pages

---
+++ src/pages/home/Home.js
@@ -0,0 +1,6 @@
+import styles from './Home.module.css';
+import React from 'react';
+
+export default function home() {
+  return <div>home</div>;
+}
---
+++ src/pages/signup/Signup.js
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+import styles from './Signup.module.css';
+
+export default function Signup() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(name, email, password);
+  };
+
+  return (
+    <form className={styles['signup-form']} onSubmit={handleSubmit}>
+      <h2>Signup</h2>
+      <label>
+        <span>Name</span>
+        <input
+          type="text"
+          onChange={(e) => setName(e.target.value)}
+          value={name}
+          required
+        />
+      </label>
+      <label>
+        <span>Email</span>
+        <input
+          type="email"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+          required
+        />
+      </label>
+      <label>
+        <span>Password</span>
+        <input
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+          required
+        />
+      </label>
+      <button className="btn">Signup</button>
+    </form>
+  );
+}
